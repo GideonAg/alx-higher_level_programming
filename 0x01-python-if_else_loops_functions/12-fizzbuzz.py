8c4799f03b11b6cc945c2bf60c3a3cd502f28813
GideonAg <gideonagbosu72@gmail.com>
2023-01-04 18:56:38 -0800

    Removed newline character

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for a in range(101):
+    for a in range(100):
         if a % 3 == 0 and a % 5 == 0:
-            print('FizzBuzz', end='')
+            print('FizzBuzz', end=' ')
         elif a % 3 == 0:
-            print('Fizz', end='')
+            print('Fizz', end=' ')
         elif a % 5 == 0:
-            print('Buzz', end='')
+            print('Buzz', end=' ')
         else:
-            print(a, end='')
+            print(a, end=' ')
+    print('FizzBuzz')
